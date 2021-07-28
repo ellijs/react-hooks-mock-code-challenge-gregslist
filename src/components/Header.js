74726858e92c423b825ab95e10c732f6922a536c
Jessica J Lee <79885174+ellijs@users.noreply.github.com>
2021-07-28 09:21:26 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
+
 import React from "react";
 import Search from "./Search";
 
-function Header() {
+const SortActivator = ({ sort:isSelected, userWantsSort }) => {
+  return (
+    <div style={{margin: "10px"}}>
+
+      <label htmlFor="sort-checkbox" >Sort by name?</label>
+      <input id="sort-checkbox" onChange={() => userWantsSort()} 
+        type="checkbox" checked={isSelected} style={{margin: "10px"}}/>
+
+    </div>
+  )
+}
+
+
+
+function Header({ sendUserInputToApp, sort, userWantsSort }) {
   return (
     <header>
       <h1>
@@ -10,7 +25,8 @@ function Header() {
         </span>
         gregslist
       </h1>
-      <Search />
+      <SortActivator sort={sort} userWantsSort={userWantsSort} />
+      <Search sendUserInputToApp={sendUserInputToApp} />
     </header>
   );
 }
